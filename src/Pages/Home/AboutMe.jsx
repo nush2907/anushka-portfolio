fcff6fc6a2cf46ed6264f57fea3633b1b485cbe1
nush2907 <137198558+nush2907@users.noreply.github.com>
2023-10-13 18:31:46 +0530

    Update AboutMe.jsx

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -9,7 +9,7 @@ export default function AboutMe() {
           <h1 className="skills-section--heading">About Me</h1>
           <p className="hero--section-description">
             I am currently a pre final year student in Vellore Institute of Technology, Chennai.
-            I am persuing Computer Science Engineering with specialization in Artificial Intelligence and Machine Learning.
+            I am pursuing Computer Science Engineering with specialization in Artificial Intelligence and Machine Learning.
             I am working towards becoming a Full-Stack Developer and also I have keen interest towards AI and ML 
             projects and research. I am also an UI/UX Researcher and I love to make designs.
           </p>
